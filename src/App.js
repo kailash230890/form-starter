b57afb38306f8f53dd9b9860e0b56150adc909ab
Kailash <53666209+kailash230890@users.noreply.github.com>
2022-02-01 21:11:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+// import DynamicForms from './components/DynamicForm';
+import Forms from './components/Forms';
+
+function App() {
+  return (
+    <div className="App">
+      <Forms />
+      {/* <DynamicForms /> */}
+    </div>
+  );
+}
+
+export default App;
